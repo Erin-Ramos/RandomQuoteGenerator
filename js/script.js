2a51a3d7fdc39c50f78210609c3070f80f484fd8
Erin Ramos <erinramos415@gmail.com>
2020-05-28 17:13:22 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance:
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/***
+ * `quotes` array
+***/
+let quotes = [
+  {
+    quote: '"Works of art make rules; rules do not make works of art."',
+    author: 'Claude Debussy'
+  };
+  {
+    quote: '"Always believe something wonderful is about to happen."',
+    author: 'Unknown'
+  };
+  {
+    quote: '"Do or do not. There is no try."',
+    author: 'Yoda'
+  };
+  {
+    quote: '"You are never too old to set another goal or to dream a new dream."',
+    author: 'C.S. Lewis'
+  };
+  {
+    quote: '"Today you are you. That is truer than true. There is no one who is youer than you!"',
+    author: 'Dr. Seuss'
+  };
+];
+
+/***
+ * `getRandomQuote` function
+***/
+function (getRandomQuote) {
+  
+}
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
